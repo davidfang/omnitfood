ff641d7b855139cbe44d0a8d7e05063b066d04df
yogee412 <liuyitaoyogee@gmail.com>
2015-12-12 11:09:18 +0800

    scroll effect done

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   /* sticky nav */
    $('.js--section-features').waypoint(function (direction) {
       if (direction == 'down') {
          $('nav').addClass('sticky');
@@ -7,5 +8,36 @@ $(document).ready(function () {
       }
    }, {
       offset: '60px;'
-   })
+   });
+
+   /* scroll */
+   $('.js-scroll-to-plans').click(function () {
+      $('html,body').animate({
+         scrollTop: $('.js--section-plans').offset().top
+      }, 1200);
+   });
+
+   $('.js-scroll-to-stars ').click(function () {
+      $('html,body').animate({
+         scrollTop: $('.js--section-features').offset().top
+      }, 1200);
+   });
+
+   /* nav scroll */
+   /* https://css-tricks.com/snippets/jquery/smooth-scrolling */
+   $(function () {
+      $('a[href*=#]:not([href=#])').click(function () {
+         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+               $('html,body').animate({
+                  scrollTop: target.offset().top
+               }, 1000);
+               return false;
+            }
+         }
+      });
+   });
+
 });
