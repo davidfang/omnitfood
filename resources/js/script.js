c4b9ba4c8b48ac2f56cbbd36b3bfdbc6ff364d8d
yogee412 <liuyitaoyogee@gmail.com>
2015-12-16 10:40:40 +0800

    modify media nav;adding resp nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -39,5 +39,45 @@ $(document).ready(function () {
          }
       });
    });
+   /* animations on scroll */
+   /* https://daneden.github.io/animate.css */
+   $(".js-wp-1").waypoint(function (direction) {
+      $(".js-wp-1").addClass("animated fadeIn");
+   }, {
+      offset: '50%'
+   });
+
+   $(".js-wp-2").waypoint(function (direction) {
+      $(".js-wp-2").addClass("animated fadeInUp");
+   }, {
+      offset: '50%'
+   });
+
+   $(".js-wp-3").waypoint(function (direction) {
+      $(".js-wp-3").addClass("animated fadeIn");
+   }, {
+      offset: '50%'
+   });
+
+   $(".js-wp-4").waypoint(function (direction) {
+      $(".js-wp-4").addClass("animated pulse");
+   }, {
+      offset: '50%'
+   });
+
+   /* mobile nav */
+   $(".js-nav-icon").click(function () {
+      var nav = $(".js-main-nav");
+      var icon = $(".js-nav-icon i");
+      if (icon.hasClass('ion-navicon-round')) {
+         icon.addClass("ion-close-round");
+         icon.removeClass("ion-navicon-round");
+      } else {
+         icon.addClass("ion-navicon-round");
+         icon.removeClass("ion-close-round");
+      }
+      nav.slideToggle(400);
+
+   });
 
 });
